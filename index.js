496a9a2747842537e73f2e5f8e556b0ead6112a0
Ahmad Shah <ah.seerat@gmail.com>
2022-10-31 08:03:43 -0400

    commit tree ba0b3ebaf235cf054529b73c76233c05bf3c531c

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById("main").remove();
+let newHeader = document.createElement('h1');
+newHeader.id = "victory";
+let victory = document.createElement('victory');
+newHeader.innerHTML = "AHMAD-SHAH is the champion";
+newHeader.appendChild(victory);
